3f150b8cd5c9f93201b1e4f4290a4d351d68854b
garylyp <garylyp@hotmail.com>
2021-03-11 16:40:50 +0800

    Add scripts to generate and delete datasets

---
+++ datasets/_init.bat
@@ -0,0 +1,12 @@
+java RandomDB Aircrafts 10000
+java RandomDB Flights 10000
+java RandomDB Employees 10000
+java RandomDB Schedule 10000
+java RandomDB Certified 10000
+
+java ConvertTxtToTbl Aircrafts
+java ConvertTxtToTbl Flights
+java ConvertTxtToTbl Employees
+java ConvertTxtToTbl Schedule
+java ConvertTxtToTbl Certified
+
---
+++ testcases/_init.bat
@@ -0,0 +1,9 @@
+java RandomDB CUSTOMER 200
+java RandomDB CART 200
+java RandomDB CARTDETAILS 200
+java RandomDB BILL 200
+
+java ConvertTxtToTbl CUSTOMER
+java ConvertTxtToTbl CART
+java ConvertTxtToTbl CARTDETAILS
+java ConvertTxtToTbl BILL
