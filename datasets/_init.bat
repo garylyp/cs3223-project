caacd3f98c7d08390c7995ca7795ff7e686ee232
garylyp <garylyp@hotmail.com>
2021-03-12 13:12:46 +0800

    Test dataset

--- datasets/_init.bat
+++ datasets/_init.bat
@@ -3,10 +3,13 @@ java RandomDB Flights 10000
 java RandomDB Employees 10000
 java RandomDB Schedule 10000
 java RandomDB Certified 10000
+java RandomDB Test1 10
+java RandomDB Test2 10
 
 java ConvertTxtToTbl Aircrafts
 java ConvertTxtToTbl Flights
 java ConvertTxtToTbl Employees
 java ConvertTxtToTbl Schedule
 java ConvertTxtToTbl Certified
-
+java ConvertTxtToTbl Test1
+java ConvertTxtToTbl Test2
